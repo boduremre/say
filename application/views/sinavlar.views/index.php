6a32d73d435ebefa13ec3a87b1451dc1f78c9937
Emre Bodur <emrebodurbto@gmail.com>
2025-02-03 16:44:22 +0300

    Update index.php

--- application/views/sinavlar.views/index.php
+++ application/views/sinavlar.views/index.php
@@ -13,7 +13,7 @@
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table table-hover datatable" data-order-column="0" data-order-direction="desc">
+                    <table class="table table-hover datatable" data-order-column="0" data-order-direction="asc">
                         <thead>
                             <tr>
                                 <th>ID</th>
