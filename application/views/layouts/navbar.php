ed760ce9dda8ef7eba6f4f110038300c65d25f60
boduremre <35879677+boduremre@users.noreply.github.com>
2025-02-03 23:01:39 +0300

    Update navbar.php

--- application/views/layouts/navbar.php
+++ application/views/layouts/navbar.php
@@ -20,63 +20,14 @@
     </div>
     <ul class="navbar-nav navbar-right">
         <li>
-            <a href="#" class="nav-link nav-link-lg fullscreen-btn">
+            <a href="#" class="nav-link nav-link-lg fullscreen-btn" data-toggle="tooltip" title="Tam Ekran" data-bs-placement="bottom">
                 <i data-feather="maximize"></i>
             </a>
         </li>
-        <li>
-            <a href="#" data-bs-toggle="dropdown" class="nav-link nav-link-lg message-toggle">
-                <!-- <i data-feather="mail" class="mailAnim"></i> -->
-                <i data-feather="mail"></i>
+        <li class="dropdown">
+            <a href="#" data-bs-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="image" src="<?php echo base_url("assets"); ?>/img/Kullanici.png" class="user-img-radious-style"> <span class="d-sm-none d-lg-inline-block"></span>
             </a>
-        </li>
-      
-        <li class="dropdown dropdown-list-toggle"><a href="#" data-bs-toggle="dropdown" class="nav-link notification-toggle nav-link-lg"><i data-feather="bell" class="bell"></i>
-            </a>
-            <div class="dropdown-menu dropdown-list dropdown-menu-right pullDown">
-                <div class="dropdown-header">
-                    Notifications
-                    <div class="float-right">
-                        <a href="#">Mark All As Read</a>
-                    </div>
-                </div>
-                <div class="dropdown-list-content dropdown-list-icons">
-                    <a href="#" class="dropdown-item dropdown-item-unread"> <span class="dropdown-item-icon bg-primary text-white"> <i class="fas
-												fa-code"></i>
-                        </span> <span class="dropdown-item-desc"> Template update is
-                            available now! <span class="time">2 Min
-                                Ago</span>
-                        </span>
-                    </a> <a href="#" class="dropdown-item"> <span class="dropdown-item-icon bg-info text-white"> <i class="far
-												fa-user"></i>
-                        </span> <span class="dropdown-item-desc"> <b>You</b> and <b>Dedik
-                                Sugiharto</b> are now friends <span class="time">10 Hours
-                                Ago</span>
-                        </span>
-                    </a> <a href="#" class="dropdown-item"> <span class="dropdown-item-icon bg-success text-white"> <i class="fas
-												fa-check"></i>
-                        </span> <span class="dropdown-item-desc"> <b>Kusnaedi</b> has
-                            moved task <b>Fix bug header</b> to <b>Done</b> <span class="time">12
-                                Hours
-                                Ago</span>
-                        </span>
-                    </a> <a href="#" class="dropdown-item"> <span class="dropdown-item-icon bg-danger text-white"> <i class="fas fa-exclamation-triangle"></i>
-                        </span> <span class="dropdown-item-desc"> Low disk space. Let's
-                            clean it! <span class="time">17 Hours Ago</span>
-                        </span>
-                    </a> <a href="#" class="dropdown-item"> <span class="dropdown-item-icon bg-info text-white"> <i class="fas
-												fa-bell"></i>
-                        </span> <span class="dropdown-item-desc"> Welcome to Jiva
-                            template! <span class="time">Yesterday</span>
-                        </span>
-                    </a>
-                </div>
-                <div class="dropdown-footer text-center">
-                    <a href="#">View All <i class="fas fa-chevron-right"></i></a>
-                </div>
-            </div>
-        </li>
-        <li class="dropdown"><a href="#" data-bs-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user"> <img alt="image" src="<?php echo base_url("assets"); ?>/img/Kullanici.png" class="user-img-radious-style"> <span class="d-sm-none d-lg-inline-block"></span></a>
             <div class="dropdown-menu dropdown-menu-right pullDown">
                 <div class="dropdown-title">
                     <?php echo $this->ion_auth->user()->row()->first_name . " " . $this->ion_auth->user()->row()->last_name; ?>
