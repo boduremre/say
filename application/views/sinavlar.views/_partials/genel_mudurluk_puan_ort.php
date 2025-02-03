b2addc210c7a3a8ebf47550a5cc0b3e3ae5dccc7
boduremre <35879677+boduremre@users.noreply.github.com>
2025-02-03 22:00:53 +0300

    Create genel_mudurluk_puan_ort.php

---
+++ application/views/sinavlar.views/_partials/genel_mudurluk_puan_ort.php
@@ -0,0 +1,26 @@
+<h6 class="text-lg-start pt-2">Genel Müdürlük Puan Ortalamaları (Puan Sıralı)</h6>
+<table class="table">
+    <thead>
+    <tr>
+        <th>Sıra</th>
+        <th>Genel Müdürlük</th>
+        <th>Öğrenci Sayısı</th>
+        <th>En Düşük Puan</th>
+        <th>En Yüksek Puan</th>
+        <th>Puan Ortalaması</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php $i=1;?>
+    <?php foreach ($istatistikler["genel_mudurluk_ortalama"] as $row) : ?>
+        <tr>
+            <td><?php echo $i++; ?></td>
+            <td><?php echo $row["genel_mudurluk_adi"]; ?></td>
+            <td><?php echo $row["ogr_sayisi"]; ?></td>
+            <td><?php echo $row["min_puan"]; ?></td>
+            <td><?php echo $row["max_puan"]; ?></td>
+            <td><?php echo $row["genel_mudurluk_ortalama"]; ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
