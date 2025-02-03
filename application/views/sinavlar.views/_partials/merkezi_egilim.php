062a8728cbb671c9eb56b3a5aef0dbeb3c0da618
Emre Bodur <emrebodurbto@gmail.com>
2025-02-03 16:44:13 +0300

    Update merkezi_egilim.php

--- application/views/sinavlar.views/_partials/merkezi_egilim.php
+++ application/views/sinavlar.views/_partials/merkezi_egilim.php
@@ -1,17 +1,17 @@
 <h6 class="text-lg-start pt-2">Merkezi Eğilim Ölçütleri (Ortalama, Medyan, Mod)</h6>
 <table class="table">
     <thead>
-    <tr>
-        <th>Ortalama (Mean)</th>
-        <th>Medyan (Median)</th>
-        <th>Mod (Mode)</th>
-    </tr>
+        <tr>
+            <th>Ortalama (Mean)</th>
+            <th>Medyan (Median)</th>
+            <th>Mod (Mode)</th>
+        </tr>
     </thead>
     <tbody>
-    <tr>
-        <td><?php echo  $istatistikler["avg_puan"]; ?></td>
-        <td><?php echo  $istatistikler["median_puan"]; ?></td>
-        <td><?php echo  $istatistikler["mode"]->puan; ?></td>
-    </tr>
+        <tr>
+            <td><?php echo  $istatistikler["avg_puan"]; ?></td>
+            <td><?php echo  $istatistikler["median_puan"]; ?></td>
+            <td><?php echo  $istatistikler["mode"]->puan . " (" . $istatistikler["mode"]->tekrar_sayisi . ")"; ?></td>
+        </tr>
     </tbody>
 </table>
