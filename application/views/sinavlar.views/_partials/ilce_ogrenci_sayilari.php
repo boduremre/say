7b4c078ae5f9b4e7bedfba96a6fedac94bf83b69
Emre Bodur <emrebodurbto@gmail.com>
2025-02-04 10:22:59 +0300

    Update ilce_ogrenci_sayilari.php

--- application/views/sinavlar.views/_partials/ilce_ogrenci_sayilari.php
+++ application/views/sinavlar.views/_partials/ilce_ogrenci_sayilari.php
@@ -1,17 +1,21 @@
 <h6 class="text-lg-start pt-2">İlçe Öğrenci Sayıları</h6>
 <table class="table">
     <thead>
-    <tr>
-        <th>İlçe</th>
-        <th>Toplam Öğrenci Sayısı</th>
-    </tr>
+        <tr>
+            <th>İlçe</th>
+            <th>Toplam Öğrenci Sayısı</th>
+        </tr>
     </thead>
     <tbody>
-    <?php foreach ($istatistikler["ogr_say_ilceler"] as $row) : ?>
-        <tr>
-            <td><?php echo $row["ilce_adi"]; ?></td>
-            <td><?php echo $row["ogr_say"]; ?></td>
+        <?php foreach ($istatistikler["ogr_say_ilceler"] as $row) : ?>
+            <tr>
+                <td><?php echo $row["ilce_adi"]; ?></td>
+                <td><?php echo $row["ogr_say"]; ?></td>
+            </tr>
+        <?php endforeach; ?>
+        <tr class="bg-success fw-bold">
+            <td>İl Geneli</td>
+            <td><?php echo $istatistikler["katilan_ogrenci_sayisi"]; ?></td>
         </tr>
-    <?php endforeach; ?>
     </tbody>
 </table>
