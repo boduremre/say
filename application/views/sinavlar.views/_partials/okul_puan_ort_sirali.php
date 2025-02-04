f1fbcdb34749adcc65deab8f7d6417f5239d279e
Emre Bodur <emrebodurbto@gmail.com>
2025-02-04 10:23:08 +0300

    Update okul_puan_ort_sirali.php

--- application/views/sinavlar.views/_partials/okul_puan_ort_sirali.php
+++ application/views/sinavlar.views/_partials/okul_puan_ort_sirali.php
@@ -1,30 +1,38 @@
 <h6 class="text-lg-start pt-2">Okul Puan Ortalamaları (Ortalama Puana Göre Sıralı)</h6>
 <table class="table">
     <thead>
-    <tr>
-        <th>Sıra</th>
-        <th>İlçe</th>
-        <th>Kurum Kodu</th>
-        <th>Kurum</th>
-        <th>Öğrenci Sayısı</th>
-        <th>En Düşük Puan</th>
-        <th>En Yüksek Puan</th>
-        <th>Puan Ortalaması</th>
-    </tr>
+        <tr>
+            <th>Sıra</th>
+            <th>İlçe</th>
+            <th>Kurum Kodu</th>
+            <th>Kurum</th>
+            <th>Öğrenci Sayısı</th>
+            <th>En Düşük Puan</th>
+            <th>En Yüksek Puan</th>
+            <th>Puan Ortalaması</th>
+        </tr>
     </thead>
     <tbody>
-    <?php $i = 1; ?>
-    <?php foreach ($istatistikler["puan_kurumlar_sirali"] as $row) : ?>
-        <tr>
+        <?php $i = 1; ?>
+        <?php foreach ($istatistikler["puan_kurumlar_sirali"] as $row) : ?>
+            <tr>
+                <td><?php echo $i++; ?></td>
+                <td><?php echo $row["ilce_adi"]; ?></td>
+                <td><?php echo $row["kurum_kodu"]; ?></td>
+                <td><?php echo $row["KURUM_ADI"]; ?></td>
+                <td><?php echo $row["ogrenci_sayisi"]; ?></td>
+                <td><?php echo $row["min_puan"]; ?></td>
+                <td><?php echo $row["max_puan"]; ?></td>
+                <td><?php echo $row["avg_puan"]; ?></td>
+            </tr>
+        <?php endforeach; ?>
+        <tr class="bg-success fw-bold">
             <td><?php echo $i++; ?></td>
-            <td><?php echo $row["ilce_adi"]; ?></td>
-            <td><?php echo $row["kurum_kodu"]; ?></td>
-            <td><?php echo $row["KURUM_ADI"]; ?></td>
-            <td><?php echo $row["ogrenci_sayisi"]; ?></td>
-            <td><?php echo $row["min_puan"]; ?></td>
-            <td><?php echo $row["max_puan"]; ?></td>
-            <td><?php echo $row["avg_puan"]; ?></td>
+            <td colspan="3">İl Geneli</td>
+            <td><?php echo $istatistikler["katilan_ogrenci_sayisi"]; ?></td>
+            <td><?php echo  $istatistikler["min_puan"]; ?></td>
+            <td><?php echo  $istatistikler["max_puan"]; ?></td>
+            <td><?php echo  $istatistikler["avg_puan"]; ?></td>
         </tr>
-    <?php endforeach; ?>
     </tbody>
 </table>
