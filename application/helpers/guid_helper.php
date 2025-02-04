81dbfabb1355db5268647a597668fcd9338f8dcf
Emre Bodur <emrebodurbto@gmail.com>
2025-02-04 12:37:43 +0300

    Update guid_helper.php

--- application/helpers/guid_helper.php
+++ application/helpers/guid_helper.php
@@ -1,12 +1,14 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-/**
- * @return string
- */
-function GUID(): string
-{
-    if (function_exists('com_create_guid') === true)
-        return trim(com_create_guid(), '{}');
+if (!function_exists('GUID')) {
+    /**
+     * @return string
+     */
+    function GUID(): string
+    {
+        if (function_exists('com_create_guid') === true)
+            return trim(com_create_guid(), '{}');
 
-    return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+        return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+    }
 }
