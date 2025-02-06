16934fa8bd69dee87dec0f3b4571bb71e6564f3e
Emre Bodur <emrebodurbto@gmail.com>
2025-02-06 13:46:38 +0300

    Update Sinavlar_model.php

--- application/models/Sinavlar_model.php
+++ application/models/Sinavlar_model.php
@@ -45,6 +45,11 @@ class Sinavlar_model extends CI_Model
         return false;
     }
 
+    /**
+     * @param array $where
+     * @param string $select
+     * @return mixed
+     */
     public function get(array $where = array(), string $select = "sinavlar.*, dersler.ders_id, dersler.ders_adi, siniflar.sinif_adi, siniflar.sinif_seviyesi"): mixed
     {
         $this->db->select($select);
@@ -55,22 +60,12 @@ class Sinavlar_model extends CI_Model
         return $this->db->get()->row();
     }
 
-    public function get_cevap_anahtari($where = array())
-    {
-        $this->db->select('sinavlar.cevap_anahtari');
-        $this->db->from($this->table_name);
-        $this->db->where($where);
-        return $this->db->get()->row()->cevap_anahtari;
-    }
-
-    public function get_soru_sayisi($where = array())
-    {
-        $this->db->select('sinavlar.soru_sayisi');
-        $this->db->from($this->table_name);
-        $this->db->where($where);
-        return $this->db->get()->row()->soru_sayisi;
-    }
-
+    /**
+     * @param array $where
+     * @param string $order
+     * @param string $select
+     * @return object|array
+     */
     public function get_all(array $where = array(), string $order = "created_at DESC", string $select = "sinavlar.*, siniflar.sinif_adi, COUNT(sp.id) as ogrenci_sayisi"): object|array
     {
         $this->db->select($select);
@@ -78,37 +73,9 @@ class Sinavlar_model extends CI_Model
         $this->db->where($where);
         $this->db->join('siniflar', 'siniflar.sinif_id = sinavlar.sinif_id');
         $this->db->join("sinav_puanlari as sp", "sinavlar.id = sp.sinav_id", "left"); // LEFT JOIN ile birleştiriyoruz
-        $this->db->group_by("sinavlar.id"); // Her sınav için toplam öğrenci sayısını hesaplıyoruz
+        $this->db->group_by("sinavlar.id");
         $this->db->order_by($order);
         return $this->db->get()->result();
-    }
-
-    public function get_all_w($where = array(), $order = "id DESC")
-    {
-        return $this->db->where($where)->order_by($order)->get($this->table_name)->result();
-    }
-
-    public function get_all_byUserID($userID)
-    {
-        $this->db->select('*')->from($this->table_name)->join('users', 'users.id = sinavlar.userID');
-        return $this->db->where($userID)->get()->result();
-    }
-
-    public function getBysinavlarKodu($sinavlarKodu)
-    {
-        $this->db->select('sinavlar.*,dersler.*')->from($this->table_name)->join('dersler', 'dersler.id = sinavlar.dersID');
-        return $this->db->where(array('sinavlar_kodu' => $sinavlarKodu))->get()->row();
-    }
-
-    public function add($data = array()): array
-    {
-        $result = $this->db->insert($this->table_name, $data);
-
-        if ($result) {
-            return array('result' => true, 'lastID' => $this->db->insert_id());
-        } else {
-            return array('result' => false, 'lastID' => 0);
-        }
     }
 
     /**
@@ -121,14 +88,12 @@ class Sinavlar_model extends CI_Model
         return $this->db->update($this->table_name, $data, $where);
     }
 
-    public function delete(array $where = array())
+    /**
+     * @param array $where
+     * @return mixed
+     */
+    public function delete(array $where = array()): mixed
     {
         return $this->db->where($where)->delete($this->table_name);
-    }
-
-    public function sendupdate($dogrulama_kodu, $data = array())
-    {
-        //print_r($data);
-        return $this->db->update('sinavlar', $data, array('dogrulama_kodu' => $dogrulama_kodu));
     }
 }
