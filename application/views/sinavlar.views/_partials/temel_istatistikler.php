349c4daacf08f677beb184a58a3c8b5bf7c703d0
Emre Bodur <emrebodurbto@gmail.com>
2025-02-06 13:46:55 +0300

    Update temel_istatistikler.php

--- application/views/sinavlar.views/_partials/temel_istatistikler.php
+++ application/views/sinavlar.views/_partials/temel_istatistikler.php
@@ -5,6 +5,7 @@
         <th>Toplam Öğrenci Sayısı</th>
         <th>Sınava Katılan Öğrenci Sayısı</th>
         <th>Sınava Katılmayan Öğrenci Sayısı</th>
+        <th>Katılım Oranı (%)</th>
         <th>Başarılı Öğrenci Sayısı <i class="fa fa-info-circle" data-toggle="tooltip" title="Sınavda 50 puan ve üzerinde puan alan öğrenciler."></i></th>
         <th>Başarısız Öğrenci Sayısı <i class="fa fa-info-circle" data-toggle="tooltip" title="Sınavda 50 puan altında puan alan öğrenciler."></i></th>
         <th>Başarı Oranı (%)</th>
@@ -15,6 +16,9 @@
         <td><?php echo $istatistikler["toplam_ogrenci_sayisi"]; ?></td>
         <td><?php echo $istatistikler["katilan_ogrenci_sayisi"]; ?></td>
         <td><?php echo $istatistikler["toplam_ogrenci_sayisi"] - $istatistikler["katilan_ogrenci_sayisi"]; ?></td>
+        <td>
+            <?php echo number_format($istatistikler["katilan_ogrenci_sayisi"] * 100 / $istatistikler["toplam_ogrenci_sayisi"], 2); ?>
+        </td>
         <td><?php echo $istatistikler["basarili_ogrenci_sayisi"]; ?></td>
         <td><?php echo $istatistikler["basarisiz_ogrenci_sayisi"]; ?></td>
         <td><?php echo $istatistikler["basari_orani"]; ?></td>
