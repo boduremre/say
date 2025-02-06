f32ee04e9a8b710c0c6711751a5a28f121474999
Emre Bodur <emrebodurbto@gmail.com>
2025-02-06 13:46:41 +0300

    Update ilce_ogrenci_sayilari.php

--- application/views/sinavlar.views/_partials/ilce_ogrenci_sayilari.php
+++ application/views/sinavlar.views/_partials/ilce_ogrenci_sayilari.php
@@ -6,28 +6,31 @@
         <th>Toplam Öğrenci Sayısı</th>
         <th>Katılan Öğrenci Sayısı</th>
         <th>Katılmayan Öğrenci Sayısı</th>
+        <th>Katılım Oranı (%)</th>
     </tr>
     </thead>
     <tbody>
-        <?php $toplam = 0; ?>
-        <?php $katilan_ogr_say = 0; ?>
-        <?php $fark = 0; ?>
-        <?php foreach ($istatistikler["ogr_say_ilceler"] as $row): ?>
-        <?php  $toplam = $toplam + $row["ogr_say"]; ?>
-        <?php  $katilan_ogr_say = $katilan_ogr_say + $row["katilan_ogr_say"]; ?>
-        <?php  $fark = $fark + $row["fark"]; ?>
-            <tr>
-                <td><?php echo $row["ilce_adi"]; ?></td>
-                <td><?php echo $row["ogr_say"]; ?></td>
-                <td><?php echo $row["katilan_ogr_say"]; ?></td>
-                <td><?php echo $row["fark"]; ?></td>
-            </tr>
-        <?php endforeach; ?>
+    <?php $toplam = 0; ?>
+    <?php $katilan_ogr_say = 0; ?>
+    <?php $fark = 0; ?>
+    <?php foreach ($istatistikler["ogr_say_ilceler"] as $row): ?>
+        <?php $toplam = $toplam + $row["ogr_say"]; ?>
+        <?php $katilan_ogr_say = $katilan_ogr_say + $row["katilan_ogr_say"]; ?>
+        <?php $fark = $fark + $row["fark"]; ?>
+        <tr>
+            <td><?php echo $row["ilce_adi"]; ?></td>
+            <td><?php echo $row["ogr_say"]; ?></td>
+            <td><?php echo $row["katilan_ogr_say"]; ?></td>
+            <td><?php echo $row["fark"]; ?></td>
+            <td><?php echo number_format($row["katilan_ogr_say"] * 100 / $row["ogr_say"], 2); ?></td>
+        </tr>
+    <?php endforeach; ?>
     <tr class="bg-success fw-bold">
         <td>İl Geneli</td>
         <td><?php echo $toplam; ?></td>
         <td><?php echo $katilan_ogr_say; ?></td>
         <td><?php echo $fark; ?></td>
+        <td><?php echo number_format($katilan_ogr_say * 100 / $toplam, 2); ?></td>
     </tr>
     </tbody>
 </table>
