955cb57444639de56b0ad56eacf38ff35ffecfc9
Emre Bodur <emrebodurbto@gmail.com>
2025-02-06 13:46:53 +0300

    Update skewness.php

--- application/views/sinavlar.views/_partials/skewness.php
+++ application/views/sinavlar.views/_partials/skewness.php
@@ -5,7 +5,7 @@
         <th>Değişim Aralığı (Ranj)</th>
         <th>Çarpıklık (Skewness) Değeri</th>
         <th>Dağılım Yönü</th>
-        <th>Dağılım Grafiği</th>
+<!--        <th>Dağılım Grafiği</th>-->
         <th>Sınav Zorluk Derecesi</th>
         <th>Açıklama</th>
     </tr>
@@ -15,11 +15,11 @@
         <td><?php echo  $istatistikler["ranj"]; ?></td>
         <td><?php echo  $istatistikler["skewness"]; ?></td>
         <td><?php echo  $istatistikler["interpret_skewness"]; ?></td>
-        <td>
-            <a href="<?php echo  $istatistikler['skewness_graph_link']; ?>" class="btn btn-primary btn-sm" role="button" target="_blank">
-                <i class="fa fa-chart-bar" data-toggle="tooltip" title="Dağılım Grafiğini Göster"></i>
-            </a>
-        </td>
+<!--        <td>-->
+<!--            <a href="--><?php //echo  $istatistikler['skewness_graph_link']; ?><!--" class="btn btn-primary btn-sm" role="button" target="_blank">-->
+<!--                <i class="fa fa-chart-bar" data-toggle="tooltip" title="Dağılım Grafiğini Göster"></i>-->
+<!--            </a>-->
+<!--        </td>-->
         <td><?php echo  $istatistikler["determine_exam_difficulty_variance_skewness"]; ?></td>
         <td><i class="fa fa-info-circle" data-toggle="tooltip" title="<?php echo $istatistikler['DİKKAT'];?>"></i></td>
     </tr>
