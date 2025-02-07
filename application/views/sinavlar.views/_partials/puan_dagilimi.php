ebf474b0bb10ca3cd35b57cb9db2e93849dad208
boduremre <35879677+boduremre@users.noreply.github.com>
2025-02-07 21:54:46 +0300

    Update puan_dagilimi.php

--- application/views/sinavlar.views/_partials/puan_dagilimi.php
+++ application/views/sinavlar.views/_partials/puan_dagilimi.php
@@ -1,27 +1,43 @@
-<h6 class="text-lg-start pt-2">Puan Dağılımı</h6>
-<div class="table-responsive">
-    <table class="table">
-        <thead>
-        <tr>
-            <th>Puan Aralığı</th>
-            <th>Öğrenci Sayısı</th>
-            <th class="text-lg-start">Puan Dağılım Grafiği</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php $firstRow = true; ?>
-        <?php foreach ($istatistikler["puan_dagilimi"] as $puan_dagilimi) : ?>
-            <tr>
-                <td><?php echo $puan_dagilimi["puan_araligi"]; ?></td>
-                <td><?php echo $puan_dagilimi["ogrenci_sayisi"]; ?></td>
-                <?php if ($firstRow) : ?>
-                    <td class="text-lg-start" rowspan="<?php echo count($istatistikler["puan_dagilimi"]); ?>">
-                        <img src="<?php echo base_url($istatistikler["puan_dagilim_grafigi"]); ?>" alt="Puan Dağılım Grafiği">
+<div class="row">
+    <div class="col-12">
+        <h6 class="text-lg-start pt-2">Puan Dağılımı</h6>
+    </div>
+    <div class="col-6">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Puan Aralığı</th>
+                    <th>Öğrenci Sayısı</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php $firstRow = true; ?>
+                <?php foreach ($istatistikler["puan_dagilimi"] as $puan_dagilimi) : ?>
+                    <tr>
+                        <td><?php echo $puan_dagilimi["puan_araligi"]; ?></td>
+                        <td><?php echo $puan_dagilimi["ogrenci_sayisi"]; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Puan Dağılım Grafiği</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>
+                        <img src="<?php echo base_url($istatistikler["puan_dagilim_grafigi"]); ?>" alt="Puan Dağılım Grafiği" style="width: 100%; height: auto;">
                     </td>
-                    <?php $firstRow = false; ?>
-                <?php endif; ?>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
+                </tr>
+                </tbody>
+            </table>
+    </div>
 </div>
