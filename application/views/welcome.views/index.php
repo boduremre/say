1d8eb49a879b0e62f38a6879d11efe848da60560
Emre Bodur <emrebodurbto@gmail.com>
2025-02-11 10:50:51 +0300

    Update index.php

--- application/views/welcome.views/index.php
+++ application/views/welcome.views/index.php
@@ -5,6 +5,14 @@
         <div class="card card-primary">
             <div class="card-header">
                 <h4>Aktif Sınavlar</h4>
+                <div class="card-header-action">
+                    <a href="<?php echo site_url('sinavlar/create'); ?>" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Yeni Sınav Ekle">
+                        <i class="fa fa-plus"></i> Yeni Sınav Ekle
+                    </a>
+                    <a href="<?php echo site_url('sinavlar/general-analysis-report'); ?>" class="btn btn-sm btn-warning" data-toggle="tooltip" title="Genel Analiz">
+                        <i class="fa fa-chart-bar"></i> Genel Analiz
+                    </a>
+                </div>
             </div>
             <div class="card-body p-3 pt-0">
                 <!-- load sınavlar partial -->
