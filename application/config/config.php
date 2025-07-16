82b4bc22f9bbc39accd4934a1b42abfb47baeb6e
boduremre <emrebodurbto@gmail.com>
2025-07-16 15:11:28 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.130/say/';
+$config['base_url'] = 'http://192.168.1.131/say/';
 
 /*
 |--------------------------------------------------------------------------
